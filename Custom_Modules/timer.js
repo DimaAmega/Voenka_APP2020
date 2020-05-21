9060912c99bbf57d7ced8b13afd52c4b3c90f4bd
DimaAmega <dima.amega1@gmail.com>
2020-05-21 19:46:52 +0300

    Changes: 	1. fix some things. 	2. add timer stdout in div.

--- Custom_Modules/timer.js
+++ Custom_Modules/timer.js
@@ -1,24 +1,28 @@
 function timer()    {
     var timer = 0,
-        timerId;
+        timerId,
+        elem = document.getElementById('timer_container');
         
     ///////////////////////////
     //      PRIVATE
     ///////////////////////////
     function updateTimer(){
         timer +=0.1;
-        timer = Number(timer.toFixed(2));
-        console.log(timer);
+        elem.innerHTML = timer.toFixed(1);
     }
     ///////////////////////////
     //      PUBLIC
     ///////////////////////////
     this.startTimer = function()  {
         timerId = setInterval(updateTimer.bind(this),100);
+        elem.style.padding = "20px";
+        elem.innerHTML = "0";
     };
     this.stopTimer = function() {  
         clearTimeout(timerId);
         var res = Number((timer - 0.1).toFixed(2));
+        elem.innerHTML = "";
+        elem.style.padding = "0";
         return res;
     };
     this.getTime = function() {
