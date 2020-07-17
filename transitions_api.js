e8de5990c212eef855fb41b8b289cf6d0972d563
DimaAmega <dima.amega1@gmail.com>
2020-07-17 19:14:47 +0300

    Changes: 	1. fixed lstest 4 transitions

--- transitions_api.js
+++ transitions_api.js
@@ -1,12 +1,25 @@
-let state = winFrame.SM.getStateByNumber('12f8v')
-console.log(state)
-state.ppo_button = 'Push'
-state.lamp5 = 'Static'
+let state = winFrame.SM.getStateByNumber('em7hu2')
+// console.log(state)
+// state.Radiostation = "Used"
+// state.Cap_of_PPO = 'Open'
+// state.ppo_button = 'Static'
+state.B_start = 'Push'
+state.CPClamp4 = 'Static'
+// console.log(winFrame.SM.getNumberByState(state))
+// state.Cap_of_PPO = 'Static'
+// state.CpcAnvil = "Move4"
+// state.Support1 = 'Static'
+// state.Support2 = 'Static'
+// state.Support3 = 'Static'
+// state.Support4 = 'Static'
 console.log(winFrame.SM.getNumberByState(state))
-state.TPK_small_very = "GoUp"
+state.TPK_small_very = 'Static'
 console.log(winFrame.SM.getNumberByState(state))
-state.lamp4 = 'Emissive'
+state.CPClamp2 = 'Emissive'
+// state.CPClamp2 = 'Static'
+// state.CPClamp4 = 'Emissive'
 console.log(winFrame.SM.getNumberByState(state))
+
 console.log(winFrame.SM.getNumberByState(state))
 state.lamp4 = 'Emissive'
 console.log(winFrame.SM.getNumberByState(state))
